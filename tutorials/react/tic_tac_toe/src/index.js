84bb0e6978710797dee492d8c1849cb73af3a96d
Kyle Reyes <mr.kylereyes@gmail.com>
2019-05-16 13:24:57 -0400

    finish game, add reset button

--- tutorials/react/tic_tac_toe/src/index.js
+++ tutorials/react/tic_tac_toe/src/index.js
@@ -2,18 +2,42 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-
-class Square extends React.Component {
-  render () {
-    return (
-      React.createElement('button', {className: 'square'}, this.props.value)
-    );
-  }
+function Square(props) {
+  return (
+    React.createElement('button', {className: 'square', onClick: props.onClick},
+      props.value
+    )
+  );
 }
+const DEFAULTSTATE = {
+  squares: Array(9).fill(null),
+  xIsNext: true,
+};
 
 class Board extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = DEFAULTSTATE
+  }
+
+  boardReset() {
+    this.setState(DEFAULTSTATE)
+  }
+
+  handleClick(i){
+    const squares = this.state.squares.slice();
+    if (calculateWinner(squares) || squares[i]) { // if winner or something already in a square do nothing
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
+  }
+
   renderSquare(i) {
-    return <Square value={i}/>;
+    return <Square value={this.state.squares[i]} onClick={()=> this.handleClick(i)}/>
   }
 
   renderBoard() {
@@ -33,12 +57,19 @@ class Board extends React.Component {
 
   render () {
     // modified from tutorial to be more DRY
-    const status = 'Next player: X';
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
     return (
       React.createElement('div', {},
         React.createElement('div', {className: 'status'},
           status,
-          this.renderBoard()
+          this.renderBoard(),
+          React.createElement('button', {onClick: ()=> this.boardReset()}, 'Reset'),
         )
       )
     );
@@ -60,6 +91,28 @@ class Game extends React.Component {
       )
     );
   }
+}
+
+function calculateWinner(squares) {
+  // TODO change this to be more dynamic if we allow more than 3x3
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < lines.length; i++){
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
 
 // ==================================================
